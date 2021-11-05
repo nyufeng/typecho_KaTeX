42324e821804eb3f99b02cf91b59b87fa0b868bf
Feng <instrye@gmail.com>
2021-11-05 10:01:48 +0800

    support typecho 1.2

--- Plugin.php
+++ Plugin.php
@@ -1,16 +1,25 @@
 <?php
+
+namespace TypechoPlugin\typecho_KaTeX;
+
+use Typecho\Plugin\Exception;
+use Typecho\Plugin\PluginInterface;
+use Typecho\Widget\Helper\Form;
+use Typecho\Widget\Helper\Form\Element\Radio;
+use Utils\Helper;
+
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 /**
  * 基于 KaTeX 的数学公式扩展
  *
- * @package KaTeX
- * @author Instrye
- * @version 0.0.1
- * @link https://github.com/Instrye/typecho_KaTeX
+ * @package typecho_KaTeX
+ * @author Feng
+ * @version 0.0.2
+ * @link https://github.com/nyufeng/typecho_KaTeX
  */
-class KaTeX_Plugin implements Typecho_Plugin_Interface
+class Plugin implements PluginInterface
 {
-    private static $KateXVersion = '0.12.0';
+    private static $KateXVersion = '0.15.1';
     private static $cdnType = 0;
 
     private static $cdnList = [
@@ -20,23 +29,14 @@ class KaTeX_Plugin implements Typecho_Plugin_Interface
 
     /**
      * 激活插件方法,如果激活失败,直接抛出异常
-     *
-     * @access public
-     * @return void
-     * @throws Typecho_Plugin_Exception
      */
     public static function activate()
     {
-        Typecho_Plugin::factory('Widget_Archive')->header = array(__CLASS__, 'loadJSCSS');
+        \Typecho\Plugin::factory('Widget_Archive')->header = array(__CLASS__, 'loadJSCSS');
     }
 
     /**
      * 禁用插件方法,如果禁用失败,直接抛出异常
-     *
-     * @static
-     * @access public
-     * @return void
-     * @throws Typecho_Plugin_Exception
      */
     public static function deactivate(){}
 
@@ -44,35 +44,38 @@ class KaTeX_Plugin implements Typecho_Plugin_Interface
      * 获取插件配置面板
      *
      * @access public
-     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @param Form $form 配置面板
      * @return void
      */
-    public static function config(Typecho_Widget_Helper_Form $form)
+    public static function config(Form $form)
     {
         /** 分类名称 */
-        $cdnType = new Typecho_Widget_Helper_Form_Element_Radio('cdn_type', ['0'=>_t("jsdelivr"),'1' =>_t("cdnjs"), '2' =>_t("本地")], '0',_t('CDN选择'));
+        $cdnType = new Radio('cdn_type', ['0'=>_t("jsdelivr"),'1' =>_t("cdnjs"), '2' =>_t("本地")], '0',_t('CDN选择'));
+        $version = new Form\Element\Text('cdn_version', null, null, _t('设置版本'));
         $form->addInput($cdnType);
+        $form->addInput($version);
     }
 
     /**
      * 个人用户的配置面板
      *
      * @access public
-     * @param Typecho_Widget_Helper_Form $form
+     * @param Form $form
      * @return void
      */
-    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+    public static function personalConfig(Form $form){}
 
     /**
      * 插件实现方法
      *
      * @access public
      * @return void
+     * @throws Exception
      */
-    public static function loadJSCSS($a, $b)
+    public static function loadJSCSS()
     {
-        $config = Helper::options()->plugin('KaTeX');
-        $cdnSrcArray = self::getCDNUrl($config->cdn_type);
+        $config = Helper::options()->plugin('typecho_KaTeX');
+        $cdnSrcArray = self::getCDNUrl($config->cdn_type, $config->version ?? self::$KateXVersion);
         $header = "\n";
         $header .= '<link rel="stylesheet" href="' . $cdnSrcArray['katex.min.css'] . '" crossorigin="anonymous">' . "\n";
         $header .= '<script defer src="' . $cdnSrcArray['katex.min.js'] . '" crossorigin="anonymous"></script>' . "\n";
@@ -81,16 +84,17 @@ class KaTeX_Plugin implements Typecho_Plugin_Interface
         echo $header;
     }
 
-    private static function getCDNUrl($cdnType){
+    private static function getCDNUrl($cdnType, $version): array
+    {
         $options = Helper::options();
         $cdnSrcArray = [];
         switch ($cdnType){
             case 0:
-                $url = str_replace('{version}', self::$KateXVersion, self::$cdnList[0]);
+                $url = str_replace('{version}', $version, self::$cdnList[0]);
 
                 break;
             case 1:
-                $url = str_replace('{version}', self::$KateXVersion, self::$cdnList[1]);
+                $url = str_replace('{version}', $version, self::$cdnList[1]);
                 break;
             default:
                 $url = $options->pluginUrl . '/KaTeX/dist/';
